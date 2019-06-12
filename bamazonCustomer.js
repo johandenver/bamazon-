541a3265585a9013e954ed1a78f28d650ae0d526
JohanDenver <48187072+johandenver@users.noreply.github.com>
2019-06-11 21:40:52 -0700

    updated .js code. only the purchase works, but still needs work on the inventory check.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,14 +14,73 @@ connection.connect(function(err){
     if (err){
         console.error("error connecting: " + err.stack);
     };
-        // throw err;
-    // insert run/search function();
-    start();
+    showItems();
 });
 
-function start() {
-    connection.query("SELECT * FROM products", function(err, res){
+function showItems() {
+    connection.query("SELECT * FROM products", function(err, data){
         if (err) throw err;
-        console.table(res);
+        console.table(data);
+        askCustomer(data);
     })
 };
+
+function askCustomer(data){
+    inquirer
+        .prompt([
+            {
+                type: "input",
+                name: "item",
+                message: "Please enter the ID of the item you want to buy.",
+                validate: function(value) {
+                    if (isNaN(value) === false) {
+                    return true;
+                    }
+                    return false;
+                }
+            },
+            {
+                type: "input",
+                name: "amount",
+                message: "How many would you like?",
+                validate: function(value) {
+                   return value > 0; 
+                  }
+            }
+        ])
+        .then(function(value){
+            var itemId = parseInt(value.item);
+            var amount = parseInt(value.amount);
+            var item = checkInventory(data, itemId, amount);
+
+            if((item)||(amount >= data[i].stock_quantity)){
+                makePurchase(itemId, amount);
+
+            }else{
+                console.log("\nWe don't have that item in stock, please choose something else.");
+            showItems();
+            }
+        });
+};
+
+function checkInventory(data, itemId, amount){
+    for (let i = 0; i < data.length; i++) {
+        var stock = data[i].stock_quantity;
+        if((data[i].item_id === itemId)||(stock >= amount)){
+            return data[i];
+        } 
+    }
+        return null;
+};
+
+function makePurchase(itemID, amount){
+    connection.query(
+        "UPDATE products SET stock_quantity = stock_quantity - ? WHERE item_id = ?",
+        [amount, itemID],
+        function(err, res) {
+            console.log("\nItem purchased! Thank you very much!");
+            showItems();
+        }
+    );
+};
+
--- schema.sql
+++ schema.sql
@@ -2,19 +2,6 @@ CREATE DATABASE bamazon_db;
 
 USE bamazon_db;
 
-CREATE TABLE auctions(
-  id INT NOT NULL AUTO_INCREMENT,
-  item_name VARCHAR(100) NOT NULL,
-  category VARCHAR(45) NOT NULL,
-  starting_bid INT default 0,
-  highest_bid INT default 0,
-  PRIMARY KEY (id)
-);
-
-CREATE DATABASE bamazon_db;
-
-USE bamazon_db;
-
 CREATE TABLE products(
     item_id INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(100) NOT NULL,
